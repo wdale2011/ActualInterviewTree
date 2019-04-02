468189122a684b8600a806ca5f2432308ca2fd37
William Dale <wmd2011@gmail.com>
2019-04-02 12:58:10 -0700

    Tree works on a basic level

--- src/components/Sub.js
+++ src/components/Sub.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import AddReport from './AddReport';
 import '../css/List.css';
 
 class Sub extends React.Component {
@@ -7,8 +8,13 @@ class Sub extends React.Component {
     this.state = {
       name: this.props.name,
       reports: [],
+      addNewReport: false,
     };
   }
+
+  addReport = () => {
+    this.setState({ addNewReport: !this.state.addNewReport });
+  };
 
   addEmployee = name => {
     let reports = this.state.reports;
@@ -16,18 +22,34 @@ class Sub extends React.Component {
     this.setState({ reports });
   };
 
+  removeEmployee = name => {
+    let reports = this.state.reports;
+    let index = reports.indexOf(name);
+    reports.splice(index, 1);
+    this.setState({ reports });
+  };
+
   render() {
     return (
       <div>
         <h3>{this.state.name}</h3>
-        <button onClick={this.state.addEmployee}>Add Report</button>
-        <button onClick={() => this.props.removeEmployee(this.state.name)}>
-          Delete Employee
-        </button>
+        {this.state.addNewReport ? (
+          <div>
+            <AddReport addEmployee={this.addEmployee} />
+            <button onClick={this.addReport}>Back</button>
+          </div>
+        ) : (
+          <div>
+            <button onClick={this.addReport}>Add Report</button>
+            <button onClick={() => this.props.removeEmployee(this.state.name)}>
+              Delete Employee
+            </button>
+          </div>
+        )}
         <ol>
           {this.state.reports.map((report, index) => (
             <li key={index}>
-              <Sub name={report} />
+              <Sub name={report} removeEmployee={this.removeEmployee} />
             </li>
           ))}
         </ol>
